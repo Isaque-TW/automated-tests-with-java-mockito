c0ebe7e4a9ff503e4cfa7c9fe6cc42a03a950318
Isaque-TW <isaque.ribeiro@thoughtworks.com>
2024-12-09 17:03:31 -0300

    Mockando Exceptions

--- src/test/java/CourseBusinessTest/ListTest.java
+++ src/test/java/CourseBusinessTest/ListTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static javax.swing.UIManager.get;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -48,15 +47,21 @@ public class ListTest {
     }
 
     @Test
-    void testMockingList_When_SizeIsCalledWithArgumentMatcher_ShouldReturnErudio() {
+    void testMockingList_When_ThrowsException() {
 
         // Given Arrange
         var list = mock(List.class);
-        when(list.get(anyInt())).thenReturn("Erudio");
 
-        assertEquals("Erudio", list.get(0));
-        assertEquals("Erudio", list.get(anyInt()));
+        // If you are using argument, all arguments
+        // have yo be provided by matchers.
+        when(list.get(anyInt())).thenThrow(new RuntimeException("Foo Bar!!"));
 
+        // When / Act
+        assertThrows(RuntimeException.class,
+                () -> {
+            // When / Act
+            list.get(anyInt());},
+                () -> "Should have throw an RuntimeException");
+        }
     }
 
-}
